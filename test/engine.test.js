99472e7a1ed06cfa736cbdf403aa42f4eb3c2c47
Dayvson Marques <dayvson.marques@gmail.com>
2026-08-07 22:13:30 -0300

    improved: modernize test file to ES6+ syntax

--- test/engine.test.js
+++ test/engine.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var test = require('node:test');
-var assert = require('node:assert');
+const test = require('node:test');
+const assert = require('node:assert');
 
 global.window = global;
 global.document = {
@@ -10,38 +10,37 @@ global.document = {
 
 require('../engine.js');
 
-var globToRegExp = global.Engine._internal.globToRegExp;
-var matchesAnyRoute = global.Engine._internal.matchesAnyRoute;
+const { globToRegExp, matchesAnyRoute } = global.Engine._internal;
 
-test('globToRegExp matches a wildcard suffix pattern', function () {
-	var regExp = globToRegExp('*.html');
+test('globToRegExp matches a wildcard suffix pattern', () => {
+	const regExp = globToRegExp('*.html');
 
 	assert.ok(regExp.test('/details.html'));
 	assert.ok(!regExp.test('/details.php'));
 });
 
-test('globToRegExp matches a wildcard prefix path pattern', function () {
-	var regExp = globToRegExp('/site/*');
+test('globToRegExp matches a wildcard prefix path pattern', () => {
+	const regExp = globToRegExp('/site/*');
 
 	assert.ok(regExp.test('/site/foo'));
 	assert.ok(!regExp.test('/other/foo'));
 });
 
-test('globToRegExp escapes regex special characters in the pattern', function () {
-	var regExp = globToRegExp('/a.b+c');
+test('globToRegExp escapes regex special characters in the pattern', () => {
+	const regExp = globToRegExp('/a.b+c');
 
 	assert.ok(regExp.test('/a.b+c'));
 	assert.ok(!regExp.test('/aXb+c'));
 });
 
-test('matchesAnyRoute returns true when any configured route matches', function () {
+test('matchesAnyRoute returns true when any configured route matches', () => {
 	assert.ok(matchesAnyRoute(['*.php', '*.html'], '/index.html'));
 });
 
-test('matchesAnyRoute returns false when no configured route matches', function () {
+test('matchesAnyRoute returns false when no configured route matches', () => {
 	assert.ok(!matchesAnyRoute(['*.php'], '/index.html'));
 });
 
-test('matchesAnyRoute returns false for an empty routes list', function () {
+test('matchesAnyRoute returns false for an empty routes list', () => {
 	assert.ok(!matchesAnyRoute([], '/index.html'));
 });
